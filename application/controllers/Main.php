9af7ddfeff24f6bea5226ca640039dfb51df29aa
Goose <goosepostbox@gmail.com>
2017-05-29 00:06:32 -0400

    Implement request middleware
    Experimental, may implement in goose_start as well.

--- application/controllers/Main.php
+++ application/controllers/Main.php
@@ -6,7 +6,12 @@ class Main extends CI_Controller {
     function __construct()
     {
         parent::__construct();
-        // $this->load->model('main_model', '', TRUE);
+        $this->load->model('main_model', '', TRUE);
+
+        $user_flag = 0;
+        $user_key = 0;
+        $api_flag = 0;
+        $this->main_model->insert_request($user_flag, $user_key, $api_flag);
     }
 
 	public function index()
--- application/models/Main_model.php
+++ application/models/Main_model.php
@@ -5,39 +5,20 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 Class main_model extends CI_Model
 {
-    function select_row($table, $id)
-    {
-        $this->db->select('*');
-        $this->db->from($table);
-        $this->db->where('id', $id);
-        $query = $this->db->get();
-        $result = $query->result_array();
-        return isset($result[0]) ? $result[0] : false;
-    }
-
-    function insert_row($table, $column)
+    function insert_request($user_flag, $user_key, $api_flag)
     {
         $data = array(
-            'column' => $column,
+            'user_flag' => $user_flag,
+            'user_key' => $user_key,
+            'api_flag' => $api_flag,
+            'ip' => $_SERVER['REMOTE_ADDR'],
+            'route_url' => parse_url($_SERVER["REQUEST_URI"], PHP_URL_PATH),
+            'full_url' => $_SERVER['REQUEST_URI'],
+            'get_data' => json_encode($_GET),
+            'post_data' => json_encode($_POST),
         );
-        $this->db->insert($table, $data);
+        $this->db->insert('request', $data);
         return $this->db->insert_id();
-    }
-
-    function update_row($table, $id, $column)
-    {
-        $data = array(
-            'column' => $column,
-            'modified' => date('Y-m-d H:i:s', time())
-        );
-        $this->db->where('id', $id);
-        $this->db->update($table, $data);
-    }
-
-    function delete_row($table, $id)
-    {
-        $this->db->where('id', $id);
-        $this->db->delete($table);
     }
 }
 ?>
