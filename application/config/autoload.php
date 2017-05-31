2c4708efeae1a95dfa22268b4a96c65daa683c1d
Goose <goosepostbox@gmail.com>
2017-05-30 20:22:39 -0400

    Turn off logging, style tweaks
    Per goose_start syncing.

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -81,22 +81,22 @@ function get_time_ago_string($time_stamp, $divisor, $time_unit) {
 
 // Flash Message
 // http://www.phpdevtips.com/2013/05/simple-session-based-flash-messages/
-function flash( $name = '', $message = '', $class = 'success' ) {
+function flash($name = '', $message = '', $class = 'success') {
     // We can only do something if the name isn't empty
-    if ( !empty( $name ) ) {
+    if (!empty( $name )) {
         // No message, create it
-        if ( !empty( $message ) && empty( $_SESSION[$name] ) ) {
-            if ( !empty( $_SESSION[$name] ) ) {
-                unset( $_SESSION[$name] );
+        if (!empty($message) && empty($_SESSION[$name])) {
+            if (!empty($_SESSION[$name])) {
+                unset($_SESSION[$name]);
             }
-            if ( !empty( $_SESSION[$name.'_class'] ) ) {
-                unset( $_SESSION[$name.'_class'] );
+            if (!empty($_SESSION[$name.'_class'])) {
+                unset($_SESSION[$name.'_class']);
             }
             $_SESSION[$name] = $message;
             $_SESSION[$name.'_class'] = $class;
         }
         // Message exists, display it
-        else if ( !empty( $_SESSION[$name] ) && empty( $message ) ) {
+        else if (!empty($_SESSION[$name]) && empty($message)) {
             $class = !empty( $_SESSION[$name.'_class'] ) ? $_SESSION[$name.'_class'] : 'success';
             echo '<div class="alert alert-' . $class . '" role="alert">' . $_SESSION[$name] . '</div>';
             unset($_SESSION[$name]);
