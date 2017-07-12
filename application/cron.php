76902fcb0d56f3811e062c384ee7f2f11032562b
Goose <goosemixes@gmail.com>
2017-07-12 12:32:22 -0400

    Fix cron production URL to .com
    I miss the xyz domains.

--- application/cron.php
+++ application/cron.php
@@ -5,7 +5,7 @@ if ($_SERVER['HTTP_HOST'] === 'localhost') {
     $base_url = 'http://localhost/karmadilemma.xyz/';
 }
 else {
-    $base_url = 'https://karmadilemma.xyz/';
+    $base_url = 'https://karmadilemma.com/';
 }
 
 chdir(__DIR__);
